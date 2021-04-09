af8fbc82d9c2963075ba0a8100b7bc7d201f2917
killabayte <killabayte.yarick@gmail.com>
2021-04-09 12:18:40 +0300

    added delete s3 bucket module

--- s3example/delete3S3bucket.py
+++ s3example/delete3S3bucket.py
@@ -2,6 +2,14 @@ import logging
 import boto3
 from botocore.exceptions import ClientError
 
+BUCKET = "nameofthebucket-04092021"
+
+def cleanBucket(bucketName):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucketName)
+    for key in bucket.objects.all():
+        key.delete()
+    return(f"Bucket: {bucketName}, successfully cleaned.")
 
 def deleteBucket(bucketName):
     try:
@@ -12,4 +20,8 @@ def deleteBucket(bucketName):
         return False
     return True
 
-deleteBucket("YourBucketName")
+cleanBucket(BUCKET)
+
+if cleanBucket:
+    print((f"Bucket: {BUCKET}, successfully cleaned."))
+    deleteBucket(BUCKET)
